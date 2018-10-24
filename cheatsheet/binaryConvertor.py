62457428c09cf5f6b144ce17048b8a20fa2f1b0e
George Reimer <georgereimer@live.ca>
2018-10-23 23:14:09 -0400

    still trying to resolve backpropagation

--- cheatsheet/binaryConvertor.py
+++ cheatsheet/binaryConvertor.py
@@ -27,20 +27,24 @@ def sigmoid_prime(z):
 
 def initWeights():
     global Wh,Wo
-    Wh = np.random.randn(INPUT_LAYER_SIZE, HIDDEN_LAYER_SIZE) * np.sqrt(2.0/INPUT_LAYER_SIZE)
-    Wo = np.random.randn(HIDDEN_LAYER_SIZE, OUTPUT_LAYER_SIZE) * np.sqrt(2.0/HIDDEN_LAYER_SIZE)
+    Wh = np.full((INPUT_LAYER_SIZE, HIDDEN_LAYER_SIZE),1.0) 
+    Wo = np.full((HIDDEN_LAYER_SIZE, OUTPUT_LAYER_SIZE),1.0) 
 
 def initBias():
     global Bh,Bo
-    Bh = np.full((1, HIDDEN_LAYER_SIZE), 0.1)
-    Bo = np.full((1, OUTPUT_LAYER_SIZE), 0.1)
+    Bh = np.full((1, HIDDEN_LAYER_SIZE), 0.5)
+    Bo = np.full((1, OUTPUT_LAYER_SIZE), 0.5)
 
 def initInput( value):
     global X,Y
+    '''
+    TODO: convert value to binary and put it into X
+    ONLY value=zero works for now
+    '''
     X = [0] * 10
-    X[value] = 1 
 
     Y = [0] * 10
+    Y[value] = 1 
 
 '''
 X    - input matrix
@@ -65,19 +69,36 @@ def backprop():
     global H,Wh,Wo,Bh,Bo,Zh,Zo,yHat,X,Y,lr
     # Layer Error
     Eo = (yHat - Y) * sigmoid_prime(Zo)
+    a = Eo * Wo
+    b = sigmoid_prime(Zh)
+    c = sigmoid_prime(Zo)
+    d = np.dot(a,b) 
+    e = a * b
     Eh = (Eo * Wo ) * sigmoid_prime(Zh)
 
     # Cost derivative for weights
     dWo = Eo * H
-    dWh = Eh * x
+    dWh = Eh * X
 
     # Update weights
     Wh -= lr * dWh
     Wo -= lr * dWo
 
+def relu_prime(Z):
+    '''
+    Z - weighted input matrix
+
+    Returns gradient of Z where all
+    negative values are set to 0 and
+    all positive values set to 1
+    '''
+    Z[Z < 0] = 0
+    Z[Z > 0] = 1
+    return Z
+
 def main():
     initWeights()
-    initInput(3)
+    initInput(0)
     initBias()
 
     feedForward()
